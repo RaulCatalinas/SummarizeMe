e5c584189deb8af2707897dd7905ec0e2d3819b0
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2024-04-25 18:51:37 +0200

    feat: implement notification system
    - Added a notification system to provide feedback to users.

---
+++ src/types/notifications.d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/indent */
+export type NotificationType = 'success' | 'error'
+
+export type GradientColors = Record<
+  NotificationType,
+  Record<
+    'firstColor' | 'secondColor' | 'thirdColor',
+    `rgba(${number},${number},${number},${number})`
+  >
+>
